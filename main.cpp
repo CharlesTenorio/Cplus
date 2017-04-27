c7fd1ac1239a4092455ce8c5e55c3f92087adf69
CharlesTenorio <charlestenorios@gmail.com>
2017-04-27 10:12:47 -0300

    C++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int number1;
+    int number2;
+    int sum;
+
+    std::cout << "Enter filst number" ;
+    std::cin >> number1;
+    std::cout<<"Enter segund number";
+    std::cin>> number2;
+    sum = number1 + number2;
+    std::cout <<"soma e "<< sum<<std::endl;
     return 0;
 }
